723cc245606cda21b3652d0bc6dea20601e607ca
sai-koushik-macha <saikoushikmacha@gmail.com>
2022-04-22 20:24:41 +0530

    use port 5000

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ app.use(express.json());
 app.use("/", routes);
 
 
-app.listen(8080, () => {
+app.listen(5000, () => {
     console.log("The application has been started at http://localhost:8080");
 });
