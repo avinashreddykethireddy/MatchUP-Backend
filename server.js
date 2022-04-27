297093b3a7e31d70a008d54e610251f59dd1fa5d
shashankmotruri <motrurishashank000@gmail.com>
2022-04-27 12:42:34 +0530

    update

--- server.js
+++ server.js
@@ -45,14 +45,13 @@ app.use(express.json());
 // routes
 app.use("/", routes);
 
-app.use(express.static('./build'))
-console.log(__dirname)
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-})
+  app.use(express.static('./build'))
+  console.log(__dirname)
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+  })
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`The application has been started at ${PORT}`);
 });
 
